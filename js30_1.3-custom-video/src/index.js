eb7ee4132c217323cd24e9d3c5b968052b0616bd
Dmitriy Konnov <iamkda@yandex.ru>
2022-02-04 13:06:40 +0300

    refactor: change structure of js

--- js30_1.3-custom-video/src/index.js
+++ js30_1.3-custom-video/src/index.js
@@ -1,21 +1,35 @@
-const videoPlayer = document.querySelector(".videoplayer");
-const videoPoster = document.querySelector(".videoplayer__poster");
-const centerPlayButton = videoPlayer.querySelector(".videoplayer__button");
-const video = videoPlayer.querySelector(".videoplayer__video");
-const progress = videoPlayer.querySelector(".videoplayer__progress-bar");
-const controls = videoPlayer.querySelector(".videoplayer__controls");
-const videoTime = videoPlayer.querySelector(".videoplayer__time");
-const playButton = videoPlayer.querySelector(".videoplayer__controls-button_type_play");
-const volumeButton = videoPlayer.querySelector(".videoplayer__controls-button_type_volume");
-const volumeReg = videoPlayer.querySelector(".videoplayer__volume-reg");
-const fullscreen = videoPlayer.querySelector(".videoplayer__controls-button_type_fullscreen");
+import {videoPlayer, videoPoster, centerPlayButton, video,
+  progress, controls, videoTime, playButton, volumeButton,
+  volumeReg, fullscreen} from "./utils/constants.js";
 
-video.volume = volumeReg.value;
+video.volume = volumeReg.value; // громкость видео = заданному при загрузке страницы range
 
 /* Чтобы видео не запускалось при кликах на панели управления */
 controls.addEventListener("click", (evt) => {
   evt.stopPropagation();
 });
+
+/* Пересчет времени из секунд в формат 00:00:00 */
+function calcTime(sec) {
+  const currentSeconds = Math.round(sec) % 60;
+  const currentMinutes =
+    Math.round(sec) / 60 >= 1 ? Math.floor((Math.round(sec) / 60) % 60) : 0;
+  const currentHours =
+    Math.round(sec) / 3600 >= 1 ? Math.floor((Math.round(sec) / 3600) % 60) : 0;
+
+  const returnTextFormatTimeUnits = (timeUnits) => {
+    if (timeUnits < 10) {
+      return `0${timeUnits}`;
+    }
+    return `${timeUnits}`;
+  };
+
+  const timeSeconds = returnTextFormatTimeUnits(currentSeconds);
+  const timeMinutes = returnTextFormatTimeUnits(currentMinutes);
+  const timeHours = returnTextFormatTimeUnits(currentHours);
+
+  return `${timeHours}:${timeMinutes}:${timeSeconds}`;
+}
 
 const playStopVideo = () => {
   if (video.paused) {
@@ -46,7 +60,10 @@ videoPlayer.addEventListener("mouseout", (evt) => {
   if (!video.paused) {
     centerPlayButton.style.visibility = "hidden";
   }
-  if (!volumeReg.classList.contains("videoplayer__volume-reg_invis") && evt.relatedTarget == document.querySelector(".page")) {
+  if (
+    !volumeReg.classList.contains("videoplayer__volume-reg_invis") &&
+    evt.relatedTarget == document.querySelector(".page")
+  ) {
     volumeReg.classList.add("videoplayer__volume-reg_invis");
   }
 });
@@ -61,37 +78,64 @@ videoPlayer.addEventListener("click", () => {
 });
 
 video.addEventListener("play", () => {
-  changeClass(centerPlayButton, "videoplayer__button_type_pause", "videoplayer__button_type_play");
-  changeClass(playButton, "videoplayer__controls-button_type_pause", "videoplayer__controls-button_type_play");
+  changeClass(
+    centerPlayButton,
+    "videoplayer__button_type_pause",
+    "videoplayer__button_type_play"
+  );
+  changeClass(
+    playButton,
+    "videoplayer__controls-button_type_pause",
+    "videoplayer__controls-button_type_play"
+  );
 });
 
 video.addEventListener("pause", () => {
-  changeClass(centerPlayButton, "videoplayer__button_type_play", "videoplayer__button_type_pause");
-  changeClass(playButton, "videoplayer__controls-button_type_play", "videoplayer__controls-button_type_pause");
+  changeClass(
+    centerPlayButton,
+    "videoplayer__button_type_play",
+    "videoplayer__button_type_pause"
+  );
+  changeClass(
+    playButton,
+    "videoplayer__controls-button_type_play",
+    "videoplayer__controls-button_type_pause"
+  );
 });
 
-// Исчезновение курсора при бездействии 
+// Исчезновение курсора при бездействии - начало
 const timeCursorVisibility = 3000;
 let timeCounter = 0;
-
 setInterval(() => {
-  if (Date.now() - timeCounter >= timeCursorVisibility && centerPlayButton.classList.contains("videoplayer__button_type_pause")) {
+  if (
+    Date.now() - timeCounter >= timeCursorVisibility &&
+    centerPlayButton.classList.contains("videoplayer__button_type_pause")
+  ) {
     video.style.cursor = "none";
     centerPlayButton.style.visibility = "hidden";
-  } else if (centerPlayButton.classList.contains("videoplayer__button_type_play")) {
+  } else if (
+    centerPlayButton.classList.contains("videoplayer__button_type_play")
+  ) {
     centerPlayButton.style.visibility = "visible";
   }
-}, 99)
+}, 99);
+// Исчезновение курсора при бездействии - конец
+
 
 video.addEventListener("timeupdate", () => {
-  const currentTime = video.currentTime / video.duration * 100;
+  const currentTime = (video.currentTime / video.duration) * 100;
   progress.style.background = `linear-gradient(to right, rgba(180, 8, 8, 0.7) 0%, rgba(180, 8, 8, 0.7) ${currentTime}%, 
   rgba(95, 95, 95, 0.7) ${currentTime}%, rgba(95, 95, 95, 0.7) 100%)`;
-  videoTime.textContent = `${calcTime(video.currentTime)} / ${calcTime(video.duration)}`;
+  videoTime.textContent = `${calcTime(video.currentTime)} / ${calcTime(
+    video.duration
+  )}`;
 });
 
+/* Реализация управления текущего времени видео на div без input[range] - начало */
 const setCurrentTimeVideo = (evt) => {
-  video.currentTime = ((evt.clientX - videoPlayer.offsetLeft) / videoPlayer.offsetWidth) * video.duration;
+  video.currentTime =
+    ((evt.clientX - videoPlayer.offsetLeft) / videoPlayer.offsetWidth) *
+    video.duration;
 };
 
 const rem = () => {
@@ -100,33 +144,42 @@ const rem = () => {
   progress.removeEventListener("mouseout", rem);
 };
 
-progress.addEventListener("mousedown",() => {
+progress.addEventListener("mousedown", () => {
   progress.addEventListener("mousemove", setCurrentTimeVideo);
   progress.addEventListener("mouseout", rem);
   progress.addEventListener("mouseup", rem);
 });
 
 progress.addEventListener("click", setCurrentTimeVideo);
+/* Реализация управления текущего времени видео на div без input[range] - конец */
 
-/* const currentSec = video.currentTime; */
-
+/* Работа со звуком - начало */
+let currentVolume; // переменная для фиксирования громкости при нажатии кнопки mute
 const toggleVolume = () => {
-  if (volumeButton.classList.contains("videoplayer__controls-button_type_volume")) {
+  if (
+    volumeButton.classList.contains("videoplayer__controls-button_type_volume")
+  ) {
     currentVolume = video.volume;
     video.volume = 0;
     volumeReg.value = 0;
-    changeClass(volumeButton, "videoplayer__controls-button_type_mute", "videoplayer__controls-button_type_volume");
+    changeClass(
+      volumeButton,
+      "videoplayer__controls-button_type_mute",
+      "videoplayer__controls-button_type_volume"
+    );
   } else {
     video.volume = currentVolume;
     volumeReg.value = currentVolume;
-    changeClass(volumeButton, "videoplayer__controls-button_type_volume", "videoplayer__controls-button_type_mute");
+    changeClass(
+      volumeButton,
+      "videoplayer__controls-button_type_volume",
+      "videoplayer__controls-button_type_mute"
+    );
   }
 };
-let currentVolume;
 
 playButton.addEventListener("click", playStopVideo);
 volumeButton.addEventListener("click", toggleVolume);
-
 
 volumeButton.addEventListener("mouseover", () => {
   volumeReg.classList.remove("videoplayer__volume-reg_invis");
@@ -135,31 +188,21 @@ volumeButton.addEventListener("mouseover", () => {
 volumeReg.addEventListener("input", () => {
   video.volume = volumeReg.value;
   if (volumeReg.value == 0) {
-    changeClass(volumeButton, "videoplayer__controls-button_type_mute", "videoplayer__controls-button_type_volume");
+    changeClass(
+      volumeButton,
+      "videoplayer__controls-button_type_mute",
+      "videoplayer__controls-button_type_volume"
+    );
   } else {
-    changeClass(volumeButton, "videoplayer__controls-button_type_volume", "videoplayer__controls-button_type_mute");
+    changeClass(
+      volumeButton,
+      "videoplayer__controls-button_type_volume",
+      "videoplayer__controls-button_type_mute"
+    );
   }
 });
+/* Работа со звуком - конец */
 
 fullscreen.addEventListener("click", () => {
-  video.webkitEnterFullScreen();
+  video.requestFullscreen();
 });
-
-function calcTime(sec) {
-  const currentSeconds = Math.round(sec) % 60;
-  const currentMinutes = (Math.round(sec) / 60 >= 1) ? Math.floor(Math.round(sec) / 60 % 60) : 0;
-  const currentHours = (Math.round(sec) / 3600 >= 1) ? Math.floor(Math.round(sec) / 3600 % 60) : 0;
-
-  const returnTextFormatTimeUnits = (timeUnits) => {
-    if (timeUnits < 10) {
-      return `0${timeUnits}`;
-    }
-    return `${timeUnits}`;
-  }
-  
-  const timeSeconds = returnTextFormatTimeUnits(currentSeconds);
-  const timeMinutes = returnTextFormatTimeUnits(currentMinutes);
-  const timeHours = returnTextFormatTimeUnits(currentHours);
-
-  return `${timeHours}:${timeMinutes}:${timeSeconds}`
-}
