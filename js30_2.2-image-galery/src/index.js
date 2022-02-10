7303cef750ebda30a2cb47646f7be03a4445ea02
Dmitriy Konnov <iamkda@yandex.ru>
2022-02-11 01:26:31 +0300

    feat: add api fetch

--- js30_2.2-image-galery/src/index.js
+++ js30_2.2-image-galery/src/index.js
@@ -2,8 +2,6 @@ import initialCards from "./utils/constants.js";
 
 const gallery = document.querySelector(".gallery__list");
 
-
-
 function createCard(cardLink) {
     const galleryItemTemplate = document.querySelector("#gallery__item");
     const galleryItem = galleryItemTemplate.content.cloneNode(true);
@@ -12,10 +10,58 @@ function createCard(cardLink) {
     gallery.append(galleryItem);
 }
 
-initialCards.forEach(item => {
+initialCards.forEach((item) => {
     createCard(item.link);
 });
 
-initialCards.forEach(item => {
-    createCard(item.link);
-});
+// fetch("https://api.unsplash.com/photos/", { /* ?client_id=HR-ZoD9QKz8-G3XXQcDdXvDyZVQg_v4KD-7P86HhD5s */
+//     method: "GET",
+//     headers: {
+//         "Accept-Version": "v1",
+//         Authorization: `Client-ID HR-ZoD9QKz8-G3XXQcDdXvDyZVQg_v4KD-7P86HhD5s`
+//     }
+// })
+// .then(res => res.json())
+// .then(res => {
+//     console.log(res);
+//     res.forEach(photo => {
+//         createCard(photo.urls.regular)
+//     })
+// })
+// .catch(() => {
+//     console.log("NO WORK");
+// })
+
+// async function getPhoto() {
+//     const res = await fetch("https://api.unsplash.com/photos/?per_page=10", {
+//         method: "GET",
+//         headers: {
+//             'Accept-Version': 'v1',
+//             Authorization: 'Client-ID HR-ZoD9QKz8-G3XXQcDdXvDyZVQg_v4KD-7P86HhD5s',
+//         },
+//     });
+//     const data = await res.json();
+//     data.forEach((photo) => {
+//         createCard(photo.urls.regular);
+//     });
+// }
+
+let query;
+let color;
+
+async function findPhoto() {
+    const res = await fetch(`https://api.unsplash.com/search/photos/?per_page=10&query=${query}&color=${color}`, {
+        method: "GET",
+        headers: {
+            'Accept-Version': 'v1',
+            Authorization: 'Client-ID HR-ZoD9QKz8-G3XXQcDdXvDyZVQg_v4KD-7P86HhD5s',
+        },
+    });
+    const data = await res.json();
+    console.log(data.results);
+    data.results.forEach((photo) => {
+        createCard(photo.urls.regular);
+    });
+}
+
+/* findPhoto(); */
