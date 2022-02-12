b25aed8804b4e4a5f49191ac8e065c088c34f239
Dmitriy Konnov <iamkda@yandex.ru>
2022-02-12 12:42:36 +0300

    feat: js add search__form animation

--- js30_2.2-image-galery/src/index.js
+++ js30_2.2-image-galery/src/index.js
@@ -1,18 +1,41 @@
 import initialCards from "./utils/constants.js";
+/* Цвета для кнопок Lang и Theme */
+const BGRU = "linear-gradient(180deg, white 0%, white 33%, blue 33%, blue 66%, red 66%, red 100%)";
+const BGEN = `linear-gradient(0deg, red 0%, red 10%, white 10%, white 20%, red 20%, red 30%, white 30%, white 40%,
+    red 40%, red 50%, white 50%, white 60%, red 60%, red 70%, white 70%, blue 100%)`;
+const DARK = "black";
+const LIGHT = "white";
 
 const gallery = document.querySelector(".gallery__list");
+const galleryItemTemplate = document.querySelector("#gallery__item"); // template-разметка карточки
 const page = document.querySelector(".page");
+const texts = Array.from(document.querySelectorAll(".page__white-text"));
+const forms = document.forms;
+const searchForm = forms["search__form"];
+const searchInput = searchForm.elements["search__input"];
+const searchButton = document.querySelector(".search__button");
 
+const langButton = document.querySelector(".buttons__button_lang");
+const langWrapper = document.querySelector(".buttons__wrapper_lang");
+const themeButton = document.querySelector(".buttons__button_theme");
+const themeWrapper = document.querySelector(".buttons__wrapper_theme");
+const textThemeButton = themeButton.querySelector(".buttons__text");
+const textThemeWrapper = themeWrapper.querySelector(".buttons__text");
 
+/* Отрисовка карточки */
+function renderCard(card) {
+    gallery.append(card);
+}
+
+/* Создание карточки */
 function createCard(cardLink) {
-    const galleryItemTemplate = document.querySelector("#gallery__item");
     const galleryItem = galleryItemTemplate.content.cloneNode(true);
     const galleryPhoto = galleryItem.querySelector(".gallery__photo");
     galleryPhoto.src = cardLink;
-    gallery.append(galleryItem);
+    renderCard(galleryItem);
 }
 
-/* Функция очистки галереи */
+/* Очистка галереи */
 function clearGallery() {
     while (gallery.lastChild) {
         gallery.removeChild(gallery.lastChild);
@@ -82,43 +105,62 @@ async function findPhoto(query) {
     });
 }
 
-const searchInput = document.forms["search__form"].elements["search__input"];
+function handleSubmit(evt) {
+    evt.preventDefault();
+    if (searchInput.value) {
+        /*         findPhoto(searchInput.value)
+                searchInput.blur();
+                searchInput.classList.remove("search__input_with-text"); */
+                
+                searchInput.focus();
+            }
+}
+
+function findLabel(element) {
+    return document.querySelector(`.${element.name}-label`);
+}
+
+searchInput.addEventListener("focus", (evt) => {
+    const label = findLabel(evt.target);
+    label.classList.add(`${evt.target.name}-label_shift`);
+    if (page.classList.contains("page_light")) {
+        label.classList.add(`${evt.target.name}-label_dark`);
+    }
+});
+
+// function setEnterListener(evt) {
+//     if (evt.key === "Enter") {
+//         sendRequest();
+//     }
+// }
+
+
+
+
 
 searchInput.addEventListener("blur", (evt) => {
     // событие blur происходит, когда фокус убирается с input
-    if (evt.target.value) {
-        evt.target.classList.add("search__input_with-text");
-    } else {
-        evt.target.classList.remove("search__input_with-text");
+    const label = findLabel(evt.target);
+    if (!evt.target.value) {
+        if (page.classList.contains("page_light")) {
+            label.classList.remove(`${evt.target.name}-label_dark`);
+        }
+        label.classList.remove(`${evt.target.name}-label_shift`);
     }
+    
+
 });
 
-const searchButton = document.querySelector(".search__button");
+/* Фокус на строке поиска при загрузке страницы */
+searchInput.focus();
 
-searchButton.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    if (searchInput.value) {
-/*         findPhoto(searchInput.value)
-        searchInput.value = "";
-        searchInput.blur();
-        searchInput.classList.remove("search__input_with-text"); */
 
-    }
-});
 
-const BGRU = "linear-gradient(180deg, white 0%, white 33%, blue 33%, blue 66%, red 66%, red 100%)";
-const BGEN = `linear-gradient(0deg, red 0%, red 10%, white 10%, white 20%, red 20%, red 30%, white 30%, white 40%,
-    red 40%, red 50%, white 50%, white 60%, red 60%, red 70%, white 70%, blue 100%)`;
 
-/* const BGLIGHT = "linear-gradient(180deg, white 0%, white 40%, black 100%)"; */
-const DARK = "black";
-const LIGHT = "white";
 
-const langButton = document.querySelector(".buttons__button_lang");
-const langWrapper = document.querySelector(".buttons__wrapper_lang");
+searchForm.addEventListener("submit", handleSubmit);
 
-const themeButton = document.querySelector(".buttons__button_theme");
-const themeWrapper = document.querySelector(".buttons__wrapper_theme");
+
 
 langButton.style.backgroundImage = BGEN; 
 langWrapper.style.backgroundImage = BGRU; 
@@ -137,17 +179,36 @@ langButton.addEventListener("click", (evt) => {
 
 });
 
-const textThemeButton = themeButton.querySelector(".buttons__text");
-const textThemeWrapper = themeWrapper.querySelector(".buttons__text");
+
 textThemeButton.style.color = DARK; 
 textThemeWrapper.style.color = LIGHT; 
 
+
+/* Смена темы страницы */
 themeButton.addEventListener("click", (evt) => {
     [textThemeButton.textContent, textThemeWrapper.textContent] = [textThemeWrapper.textContent, textThemeButton.textContent];
     [textThemeButton.style.color, textThemeWrapper.style.color] = [textThemeWrapper.style.color, textThemeButton.style.color];
     [themeButton.style.backgroundColor, themeWrapper.style.backgroundColor] = [themeWrapper.style.backgroundColor, themeButton.style.backgroundColor];
     page.classList.toggle("page_light");
+    texts.forEach(item => {
+        item.classList.toggle("page__white-text");
+    });
+    /* Проходим по всем формам, ищем все инпуты типа text и меняем цвет текста их лэйблов на противоположный */
+    Array.from(forms).forEach(form => {
+        Array.from(form.elements).forEach(element => {
+            if (element.type === "text") {
+                if (element.value) {
+                    const label = findLabel(element);
+                    label.classList.toggle(`${element.name}-label_dark`);
+                }
+            }
+        })
+    });
+
 });
+
+
+
 
 
 
