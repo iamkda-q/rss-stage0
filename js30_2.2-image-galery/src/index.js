fa7ba050ed50e22e15a7bd7d1ec37d66874faa0e
Dmitriy Konnov <iamkda@yandex.ru>
2022-02-13 17:34:00 +0300

    refactor: refactor js code

--- js30_2.2-image-galery/src/index.js
+++ js30_2.2-image-galery/src/index.js
@@ -1,32 +1,32 @@
-import {initialCards, languages} from "./utils/constants.js";
-/* Цвета для кнопок Lang и Theme */
-const BGRU = "linear-gradient(180deg, white 0%, white 33%, blue 33%, blue 66%, red 66%, red 100%)";
-const BGEN = `linear-gradient(0deg, red 0%, red 10%, white 10%, white 20%, red 20%, red 30%, white 30%, white 40%,
-    red 40%, red 50%, white 50%, white 60%, red 60%, red 70%, white 70%, blue 100%)`;
-const DARK = "black";
-const LIGHT = "white";
-/* Галерея */
-const gallery = document.querySelector(".gallery__list");
-const galleryItemTemplate = document.querySelector("#gallery__item"); // template-разметка карточки
-const page = document.querySelector(".page");
-/* Поиск */
-const forms = document.forms;
-const searchForm = forms["search__form"];
-const searchInput = searchForm.elements["search__input"];
-const clearButton = document.querySelector(".search__clear-button");
-/* Язык */
-const texts = Array.from(document.querySelectorAll(".page__white-text"));
-const langButton = document.querySelector(".buttons__button_lang");
-const langWrapper = document.querySelector(".buttons__wrapper_lang");
-/* Тема */
-const themeButton = document.querySelector(".buttons__button_theme");
-const themeWrapper = document.querySelector(".buttons__wrapper_theme");
+import {
+    languages,
+    serverErrors,
+    BGRU,
+    BGEN,
+    DARK,
+    LIGHT,
+    header,
+    headerLogo,
+    gallery,
+    galleryItemTemplate,
+    page,
+    forms,
+    searchForm,
+    searchInput,
+    clearButton,
+    texts,
+    langButton,
+    langWrapper,
+    themeButton,
+    themeWrapper,
+} from "./utils/constants.js";
 
-langButton.style.backgroundImage = BGEN; 
-langWrapper.style.backgroundImage = BGRU; 
 
-themeButton.style.backgroundColor = LIGHT; 
-themeWrapper.style.backgroundColor = DARK; 
+langButton.style.backgroundImage = BGEN;
+langWrapper.style.backgroundImage = BGRU;
+
+themeButton.style.backgroundColor = LIGHT;
+themeWrapper.style.backgroundColor = DARK;
 
 /* Отрисовка карточки */
 function renderCard(card) {
@@ -45,56 +45,50 @@ function createCard(cardLink) {
 function clearGallery() {
     while (gallery.lastChild) {
         gallery.removeChild(gallery.lastChild);
-      }
+    }
 }
 
-initialCards.forEach((item) => {
-    renderCard(createCard(item.link));
-});
-initialCards.forEach((item) => {
-    renderCard(createCard(item.link));
-});
-initialCards.forEach((item) => {
-    renderCard(createCard(item.link));
-});
+async function getPhoto() {
+    const res = await fetch("https://api.unsplash.com/photos/?per_page=15", {
+        method: "GET",
+        headers: {
+            "Accept-Version": "v1",
+            Authorization:
+                "Client-ID HR-ZoD9QKz8-G3XXQcDdXvDyZVQg_v4KD-7P86HhD5s",
+        },
+    });
+    if (res.ok) {
+        const data = await res.json();
+        data.forEach((photo) => {
+            renderCard(createCard(photo.urls.regular));
+        });
+    } else {
+        return Promise.reject(res);
+    }
+}
 
-// fetch("https://api.unsplash.com/photos/", { /* ?client_id=HR-ZoD9QKz8-G3XXQcDdXvDyZVQg_v4KD-7P86HhD5s */
-//     method: "GET",
-//     headers: {
-//         "Accept-Version": "v1",
-//         Authorization: `Client-ID HR-ZoD9QKz8-G3XXQcDdXvDyZVQg_v4KD-7P86HhD5s`
-//     }
-// })
-// .then(res => res.json())
-// .then(res => {
-//     console.log(res);
-//     res.forEach(photo => {
-//         renderCard(createCard(photo.urls.regular))
-//     })
-// })
-// .catch(() => {
-//     console.log("NO WORK");
-// })
+function errorHandler(errorStatus) {
+    if (Object.keys(serverErrors).includes(String(errorStatus))) {
+        return serverErrors[errorStatus];
+    }
+    return "Серверная ошибка.";
+}
 
-// async function getPhoto() {
-//     const res = await fetch("https://api.unsplash.com/photos/?per_page=12", {
-//         method: "GET",
-//         headers: {
-//             'Accept-Version': 'v1',
-//             Authorization: 'Client-ID HR-ZoD9QKz8-G3XXQcDdXvDyZVQg_v4KD-7P86HhD5s',
-//         },
-//     });
-//     const data = await res.json();
-//     data.forEach((photo) => {
-//         renderCard(createCard(photo.urls.regular));
-//     });
-// }
+function returnError(err) {
+    return `${errorHandler(err.status)} Номер ошибки - ${
+        err.status ? err.status : "неизвестен"
+    }. Всего хорошего!`;
+}
 
+/* 12 фото при загрузке страницы */
+getPhoto().catch((err) => {
+    console.log(returnError(err));
+});
 /* let color; &color=${color} */
 
 async function findPhoto(query) {
     const res = await fetch(
-        `https://api.unsplash.com/search/photos/?per_page=12&query=${query}`,
+        `https://api.unsplash.com/search/photos/?per_page=18&query=${query}`,
         {
             method: "GET",
             headers: {
@@ -107,18 +101,18 @@ async function findPhoto(query) {
     const data = await res.json();
     clearGallery();
     data.results.forEach((photo) => {
-        renderCard(createCard(photo.urls.regular))
+        renderCard(createCard(photo.urls.regular));
     });
 }
 
 function handleSubmit(evt) {
     evt.preventDefault();
     if (searchInput.value) {
-        /*         findPhoto(searchInput.value)
-                searchInput.blur();
-                searchInput.classList.remove("search__input_with-text"); */
-                searchInput.focus();
-            }
+        findPhoto(searchInput.value);
+        searchInput.blur();
+        searchInput.classList.remove("search__input_with-text");
+        searchInput.focus();
+    }
 }
 
 function findLabel(element) {
@@ -151,9 +145,9 @@ function clearInput(btn, input) {
 
 searchInput.addEventListener("input", (evt) => {
     if (evt.target.value) {
-        handleInputFull(clearButton , evt.target)
+        handleInputFull(clearButton, evt.target);
     } else {
-        handleInputClear(clearButton , evt.target)
+        handleInputClear(clearButton, evt.target);
     }
 });
 
@@ -204,11 +198,11 @@ function changeLang() {
 /* Смена языка */
 langButton.addEventListener("click", (evt) => {
     changeLang();
-    [langButton.style.backgroundImage, langWrapper.style.backgroundImage] = [langWrapper.style.backgroundImage, langButton.style.backgroundImage];
+    [langButton.style.backgroundImage, langWrapper.style.backgroundImage] = [
+        langWrapper.style.backgroundImage,
+        langButton.style.backgroundImage,
+    ];
 });
-
-const header = document.querySelector(".header");
-const headerLogo = header.querySelector(".header__logo");
 
 /* Смена темы страницы */
 themeButton.addEventListener("click", (evt) => {
@@ -219,47 +213,60 @@ themeButton.addEventListener("click", (evt) => {
         textElement.classList.toggle("buttons__text_color_dark");
         textElement.classList.toggle("buttons__text_color_light");
     }
-    [textThemeButton.textContent, textThemeWrapper.textContent] = [textThemeWrapper.textContent, textThemeButton.textContent];
+    [textThemeButton.textContent, textThemeWrapper.textContent] = [
+        textThemeWrapper.textContent,
+        textThemeButton.textContent,
+    ];
     toggleTextTheme(textThemeButton);
     toggleTextTheme(textThemeWrapper);
     /* Кнопка и обертка меняются текстами */
-    [themeButton.style.backgroundColor, themeWrapper.style.backgroundColor] = [themeWrapper.style.backgroundColor, themeButton.style.backgroundColor];
+    [themeButton.style.backgroundColor, themeWrapper.style.backgroundColor] = [
+        themeWrapper.style.backgroundColor,
+        themeButton.style.backgroundColor,
+    ];
     page.classList.toggle("page_light");
     header.classList.toggle("header_light");
     headerLogo.classList.toggle("header__logo_light");
-    texts.forEach(item => {
+    texts.forEach((item) => {
         item.classList.toggle("page__white-text");
     });
     /* Проходим по всем формам, ищем все инпуты типа text и меняем цвет текста их лэйблов на противоположный */
-    Array.from(forms).forEach(form => {
-        Array.from(form.elements).forEach(element => {
+    Array.from(forms).forEach((form) => {
+        Array.from(form.elements).forEach((element) => {
             if (element.type === "text") {
                 if (element.value) {
                     const label = findLabel(element);
                     label.classList.toggle(`${element.name}-label_dark`);
                 }
             }
-        })
+        });
     });
-
 });
 
+console.log(`
+"Самооценка для проверяющего"
+Score: 60 / 60.
 
+[10/10] - Вёрстка
+- [+] на странице есть несколько фото и строка поиска (+5)
+- [+] в футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс (+5)
 
+[10/10] - При загрузке приложения на странице отображаются полученные от API изображения
 
+[10/10] - Если в поле поиска ввести слово и отправить поисковый запрос, 
+на странице отобразятся изображения соответствующей тематики, если такие данные предоставляет API
 
+[30/30] - Поиск
+- [+] При открытии приложения курсор находится в поле ввода (+5)
+- [+] Есть placeholder (+5)
+- [+] Автозаполнение поля ввода отключено (нет выпадающего списка с предыдущими запросами) (+5)
+- [+] Поисковый запрос можно отправить нажатием клавиши Enter (+5)
+- [+] После отправки поискового запроса и отображения результатов поиска, поисковый запрос продолжает отображаться в поле ввода (+5)
+- [+] В поле ввода есть крестик при клике по которому поисковый запрос из поля ввода удаляется и отображается placeholder (+5)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log();
+[10/10] - Дополнительно
+- [+] Адаптивная верстка, при уменьшении ширины экрана меняются сетка галереи и размеры некоторых элементов страницы
+- [+] Есть возможность выбора языка страницы (русский/английский)
+- [+] Есть возможность выбора темы страницы (светлая/темная)
+- [+] Верстка поисковой строки с плавными анимациями
+- [+] Обработка серверных ошибок`);
--- js30_2.2-image-galery/src/utils/constants.js
+++ js30_2.2-image-galery/src/utils/constants.js
@@ -1,67 +1,88 @@
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    }
-  ];
-
-  const ru = {
-    ".header__title" : "Галерея с API Unsplash",
+const ru = {
+    ".header__title": "Галерея с API Unsplash",
     ".search__input": "Например, котики...",
-    ".search__input-label" : "Поиск фото",
-    ".footer__copyright" : "2022 Коннов Дмитрий",
-    ".footer__link" : "Мой github",
-    ".buttons__text_en" : "Рус",
-    ".buttons__text_ru" : "Англ",
-    ".buttons__text_color_light" : "Ночь",
-    ".buttons__text_color_dark" : "День",
-  };
+    ".search__input-label": "Поиск фото",
+    ".footer__copyright": "2022 Коннов Дмитрий",
+    ".footer__link": "Мой github",
+    ".buttons__text_en": "Рус",
+    ".buttons__text_ru": "Англ",
+    ".buttons__text_color_light": "Ночь",
+    ".buttons__text_color_dark": "День",
+};
 
-  const en = {
-    ".header__title" : "Gallery with API Unsplash",
+const en = {
+    ".header__title": "Gallery with API Unsplash",
     ".search__input": "For example, cats...",
-    ".search__input-label" : "Photo search",
-    ".footer__copyright" : "2022 Konnov Dmitriy",
-    ".footer__link" : "My github",
-    ".buttons__text_en" : "En",
-    ".buttons__text_ru" : "Rus",
-    ".buttons__text_color_light" : "Dark",
-    ".buttons__text_color_dark" : "Light",
-  };
+    ".search__input-label": "Photo search",
+    ".footer__copyright": "2022 Konnov Dmitriy",
+    ".footer__link": "My github",
+    ".buttons__text_en": "En",
+    ".buttons__text_ru": "Rus",
+    ".buttons__text_color_light": "Dark",
+    ".buttons__text_color_dark": "Light",
+};
 
-  const languages = {
+const languages = {
     ru: ru,
     en: en,
-  };
+};
 
-  
+const serverErrors = {
+    400: "Некорректный запрос на сервер",
+    401: "Извините, но по какой-то причине вам отказано в доступе.",
+    403: "Извините, но по какой-то причине вам отказано в доступе.",
+    404: "Запрашиваемый вами ресурс отсутствует.",
+    500: "Внутренняя ошибка сервера.",
+};
 
-  export {initialCards, languages};
+/* Цвета для кнопок Lang и Theme */
+const BGRU =
+    "linear-gradient(180deg, white 0%, white 33%, blue 33%, blue 66%, red 66%, red 100%)";
+const BGEN = `linear-gradient(0deg, red 0%, red 10%, white 10%, white 20%, red 20%, red 30%, white 30%, white 40%,
+red 40%, red 50%, white 50%, white 60%, red 60%, red 70%, white 70%, blue 100%)`;
+const DARK = "black";
+const LIGHT = "white";
+
+/* Header */
+const header = document.querySelector(".header");
+const headerLogo = header.querySelector(".header__logo");
+
+/* Галерея */
+const gallery = document.querySelector(".gallery__list");
+const galleryItemTemplate = document.querySelector("#gallery__item"); // template-разметка карточки
+const page = document.querySelector(".page");
+/* Поиск */
+const forms = document.forms;
+const searchForm = forms["search__form"];
+const searchInput = searchForm.elements["search__input"];
+const clearButton = document.querySelector(".search__clear-button");
+/* Язык */
+const texts = Array.from(document.querySelectorAll(".page__white-text"));
+const langButton = document.querySelector(".buttons__button_lang");
+const langWrapper = document.querySelector(".buttons__wrapper_lang");
+/* Тема */
+const themeButton = document.querySelector(".buttons__button_theme");
+const themeWrapper = document.querySelector(".buttons__wrapper_theme");
+
+export {
+    languages,
+    serverErrors,
+    BGRU,
+    BGEN,
+    DARK,
+    LIGHT,
+    header,
+    headerLogo,
+    gallery,
+    galleryItemTemplate,
+    page,
+    forms,
+    searchForm,
+    searchInput,
+    clearButton,
+    texts,
+    langButton,
+    langWrapper,
+    themeButton,
+    themeWrapper,
+};
